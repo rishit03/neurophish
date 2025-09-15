ae385876666da7803721c0007dc5f5cc201cb77e
Rishit Goel <rgoel.goel03@gmail.com>
2025-09-15 02:27:56 -0700

    some change

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: "class", // <-- important
+  darkMode: "class",
   content: ["./index.html", "./src/**/*.{ts,tsx}"],
   theme: {
     extend: {
@@ -8,11 +8,11 @@ export default {
         ink: {
           900: "rgb(var(--bg-900) / <alpha-value>)",
           800: "rgb(var(--bg-800) / <alpha-value>)",
-        }
+        },
       },
       borderColor: {
         subtle: "rgb(var(--border) / <alpha-value>)",
-      }
+      },
     },
   },
   plugins: [],
